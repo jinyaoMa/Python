85bd76883752e811665f86073c2dbbd8f148a1d4
jinyaoMa <jinyao.ma@outlook.com>
2017-01-18 18:43:47 -0500

    ..

--- DIY.py
+++ DIY.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 import random
+import string
 
 class VCodeGenerator(object):
     __strHexMaxClrOfFont = 0xffffff
@@ -15,10 +16,10 @@ class VCodeGenerator(object):
     __dblFontSize = 0.75
     
     '''
-    ' Initialize VCodeGenerator
-    ' @param fontPath - path of font file e.g 'C:\Windows\Fonts\Arial.ttf'
-    ' @param numOfChar - number of characters to be generated e.g 4
-    ' @param height - height of verification code image e.g. 64
+    # Initialize VCodeGenerator
+    # @param fontPath - path of font file e.g 'C:\Windows\Fonts\Arial.ttf'
+    # @param numOfChar - number of characters to be generated e.g 4
+    # @param height - height of verification code image e.g. 64
     '''
     def __init__(self, fontPath, numOfChars, height):
         self.__strFontPath = fontPath
@@ -28,8 +29,8 @@ class VCodeGenerator(object):
         self.__imgVerificationCode = Image.new('RGB', (self.__intWidth, self.__intHeight), "#" + hex(self.__strHexMaxClrOfBg)[2:])
         
     '''
-    ' Set Font Size
-    ' @param fontSize - decimal/precentage due to image height e.g. 0.5
+    # Set Font Size
+    # @param fontSize - decimal/precentage due to image height e.g. 0.5
     '''
     def setFontSize(self, fontSize):
         if (fontSize > 1.0):
@@ -37,42 +38,40 @@ class VCodeGenerator(object):
         self.dblFontSize = fontSize
         
     '''
-    ' Get Font Size
+    # Get Font Size
     '''
     def getFontSize(self):
         return self.dblFontSize
 
     '''
-    ' Generate a random character include letters & digits
+    # Generate a random character include letters & digits
     '''
     def __generateRandChar(self):
-        finalChar = ''
-        firstRan = random.randint(65, 90);
-        if (firstRan % 2):
-            firstRan = random.randint(65, 90) % 10
-            finalChar = str(firstRan)
+        finalChar = random.randint(0, 1)
+        if (finalChar):
+            finalChar = str(random.randint(0, 9))
         else:
-            finalChar = chr(firstRan)
+            finalChar = random.choice(string.ascii_letters)
         return finalChar
 
     '''
-    ' Generate a random background color
+    # Generate a random background color
     '''
     def __generateRandBgColor(self):
         return (random.randint(self.__strHexMinClrOfBg, self.__strHexMaxClrOfBg))
 
     '''
-    ' Generate a random font color
+    # Generate a random font color
     '''
     def __generateRandFontColor(self):
         return (random.randint(self.__strHexMinClrOfFont, self.__strHexMaxClrOfFont))
 
     '''
-    ' Generate final verification code image
-    ' @return code - codes to verify
+    # Generate final verification code image
+    # @return code - codes to verify
     '''
     def generate(self):
-        code = []
+        code = ""
         marginBetweenChar = self.__intHeight * (1 - self.__dblFontSize)
         imgfntFont = ImageFont.truetype(self.__strFontPath, int(self.__intHeight * self.__dblFontSize))
         imgdrwDraw = ImageDraw.Draw(self.__imgVerificationCode)
@@ -82,7 +81,7 @@ class VCodeGenerator(object):
         for s in range(self.__intNumOfChars):
             strRanChar = self.__generateRandChar()
             imgdrwDraw.text((self.__intHeight * s + marginBetweenChar, marginBetweenChar / 2), strRanChar, font = imgfntFont, fill = self.__generateRandFontColor())
-            code.extend(strRanChar)
+            code += strRanChar
         self.__imgVerificationCode = self.__imgVerificationCode.filter(ImageFilter.BLUR)
         self.__imgVerificationCode.save('code.jpg', 'jpeg')
         return code
